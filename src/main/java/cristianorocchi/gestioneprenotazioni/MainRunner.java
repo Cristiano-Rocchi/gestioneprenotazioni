a01c52299c59d6ea5913cef8e693ad8e4bfefb86
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-06 15:54:38 +0200

    fixed mainrunner

--- src/main/java/cristianorocchi/gestioneprenotazioni/MainRunner.java
+++ src/main/java/cristianorocchi/gestioneprenotazioni/MainRunner.java
@@ -1,10 +1,8 @@
 package cristianorocchi.gestioneprenotazioni;
 
-
-
 import cristianorocchi.gestioneprenotazioni.entities.*;
 import cristianorocchi.gestioneprenotazioni.enums.TipoPostazione;
-import cristianorocchi.gestioneprenotazioni.repositories.*;
+import cristianorocchi.gestioneprenotazioni.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,46 +12,46 @@ import java.util.Arrays;
 
 @Component
 public class MainRunner implements CommandLineRunner {
-    @Autowired
-    private final UtenteRepository utenteRepository;
-    @Autowired
-    private final EdificioRepository edificioRepository;
-    @Autowired
-    private final PostazioneRepository postazioneRepository;
-    @Autowired
-    private final PrenotazioneRepository prenotazioneRepository;
 
-    public MainRunner(UtenteRepository utenteRepository, EdificioRepository edificioRepository,
-                             PostazioneRepository postazioneRepository, PrenotazioneRepository prenotazioneRepository) {
-        this.utenteRepository = utenteRepository;
-        this.edificioRepository = edificioRepository;
-        this.postazioneRepository = postazioneRepository;
-        this.prenotazioneRepository = prenotazioneRepository;
-    }
+    @Autowired
+    private UtenteService utenteService;
+
+    @Autowired
+    private EdificioService edificioService;
+
+    @Autowired
+    private PostazioneService postazioneService;
+
+    @Autowired
+    private PrenotazioneService prenotazioneService;
 
     @Override
     public void run(String... args) throws Exception {
-       /*EDIFICI*/
+        /* EDIFICI */
         Edificio edificio1 = new Edificio("Edificio A", "Via Roma 1", "Milano");
         Edificio edificio2 = new Edificio("Edificio B", "Via Milano 5", "Torino");
-        edificioRepository.saveAll(Arrays.asList(edificio1, edificio2));
+        edificioService.saveEdificio(edificio1);
+        edificioService.saveEdificio(edificio2);
 
-       /*POSTAZIONI*/
+        /* POSTAZIONI */
         Postazione postazione1 = new Postazione("Postazione Privata ", TipoPostazione.PRIVATO, 1, edificio1);
         Postazione postazione2 = new Postazione("Postazione Open Space ", TipoPostazione.OPENSPACE, 10, edificio1);
+        postazioneService.savePostazione(postazione1);
+        postazioneService.savePostazione(postazione2);
 
-        postazioneRepository.saveAll(Arrays.asList(postazione1, postazione2));
+        /* UTENTI */
+        Utente utente1 = new Utente("RAFA99", "Rafael", "Leao", "Rafa@libero.it");
+        Utente utente2 = new Utente("Theo", "Theo", "Hernandez", "Theo@libero.it");
+        utenteService.saveUtente(utente1);
+        utenteService.saveUtente(utente2);
 
-        /*UTENTI*/
-        Utente utente1 = new Utente("RAFA99", "Rafael","Leao" ,"Rafa@libero.it");
-        Utente utente2 = new Utente("Theo", "Theo", "Hernandez","Theo@libero.it");
-        utenteRepository.saveAll(Arrays.asList(utente1, utente2));
-
-        /*PRENOTAZIONI*/
+        /* PRENOTAZIONI */
         Prenotazione prenotazione1 = new Prenotazione(LocalDate.now(), utente1, postazione1);
         Prenotazione prenotazione2 = new Prenotazione(LocalDate.now().plusDays(1), utente2, postazione2);
-        prenotazioneRepository.saveAll(Arrays.asList(prenotazione1, prenotazione2));
+        prenotazioneService.savePrenotazione(prenotazione1);
+        prenotazioneService.savePrenotazione(prenotazione2);
 
         System.out.println("FUNZIONA!");
     }
 }
+
